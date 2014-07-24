f5acbeb49a880690fd3fbf4e45bad74cb6996ae3
yong <zhiyong@qq.com>
2014-07-24 10:36:56 +0800

    fix 比分网页

--- src/main/java/com/huake/web/controller/mgr/ChatController.java
+++ src/main/java/com/huake/web/controller/mgr/ChatController.java
@@ -1,17 +1,23 @@
 package com.huake.web.controller.mgr;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.huake.entity.Member;
 import com.huake.service.member.MemberService;
+import com.huake.util.RemoteParser;
 
 @Controller
 @RequestMapping(value = "/chat")
@@ -25,6 +31,12 @@ public class ChatController {
 	
 	@Autowired
 	private MemberService memberService;
+	
+	@Autowired
+	private RemoteParser remoteParser;
+	
+	@Value("#{envProps.livePath}")
+	private String livePath;
 	
 	/**
 	 * 首页
@@ -44,8 +56,8 @@ public class ChatController {
 		return "/chat/index";
 	}
 	
-	@RequestMapping(value="liveRoom",method = RequestMethod.GET)
-	public String liveRoom(Model model){
+	@RequestMapping(value="liveRoom/{id}",method = RequestMethod.GET)
+	public String liveRoom(Model model,@PathVariable final String id){
 		Member member = getCurrentMember();
 		if(member == null){
 			Member mem =new Member();
@@ -55,6 +67,24 @@ public class ChatController {
 			model.addAttribute("member",member);
 			model.addAttribute("channelName",CHAT_ZHIBO);
 		}
+		
+		String url=livePath+id;
+		Map<String,String> tagMap=new HashMap<String,String>();
+		Map<String,String> removeMap=new HashMap<String,String>();
+		tagMap.put("id=live-matches", "div");
+		removeMap.put("class=title", "div");
+		removeMap.put("class=left", "th");
+		String htmlContent="";//捕捉内容
+		try {
+//			htmlContent=remoteParser.parseHtmlContent(url, tagMap, null, "utf-8");
+			htmlContent=remoteParser.parseHtmlContent("http://192.168.1.50/specials/test1.html", tagMap, removeMap, "utf-8");
+
+		} catch (Exception e) {
+			logger.debug("***********************************异常************");
+		}
+		
+		model.addAttribute("htmlContent",htmlContent);
+		model.addAttribute("id",id);
 		return "/chat/liveRoom";
 	}
 	/**
